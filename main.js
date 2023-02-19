c16c8bb9a1df07336a7a368833ec4aa576927726
Ron Friedman <ron852719@icloud.com>
2023-02-18 23:48:33 -0800

    - Added "Nearby Memos" Feature

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-deltaLat = 0.06; // Boundaries to load markers within
-deltaLong = 0.06;
+deltaLat = 0.07; // Boundaries to load markers within
+deltaLong = 0.07;
 
 lastKnownLocation = null; // Tracks last-known location
 fingerprintingBlocked = false;
@@ -42,44 +42,89 @@ function updateMapMarkers(locationEvent) {
         .then(response => response.json())
         .then(jsonResponse => {
             $("path.leaflet-interactive").remove(); // Clear all circleMarkers so we can redraw them
+            $("#nearbyMenu").empty();               // Clear nearbyMenu so we can update it
             jsonResponse.forEach(function(marker) {  
                 placeMarker(marker);
+                addDropdownItem(marker);
             });
         });
     });
 }
 
-// Draws a circle marker representing a memo
+placedMarkers = {}; // Stores references to placed markers
+
+// Draws a circle marker representing a memo, and adds it to placedMarkers
 function placeMarker(markerJson) {
-    if (markerJson.author_hash) { // Current user is author of marker—: show delete button.
-        var circleMarker = L.circleMarker([markerJson.latitude, markerJson.longitude], {stroke: true, fillOpacity: 0.75, radius: 16, color: "#fcc201"}).addTo(map);
+    if (markerJson.author_hash) { // Current user is author of marker: show delete button.
+        var circleMarker = L.circleMarker([markerJson.latitude, markerJson.longitude], 
+            {stroke: true, fillOpacity: 0.75, radius: 16, color: "#fcc201"})
+            .addTo(map);
         circleMarker.bindPopup("<div class=\"text-center\"><p class=\"lead\">" + 
             markerJson.message + 
             "</p><p> Memo Date: " +
             markerJson.creation_date +
             "</p><button class=\"btn btn-danger\" onclick=\"deleteMemo(" + markerJson.marker_id + ")\">Delete Memo</button></div>");
     } else { // Current user is not author of marker
-        var circleMarker = L.circleMarker([markerJson.latitude, markerJson.longitude], {stroke: true, fillOpacity: 0.75, radius: 16}).addTo(map);
+        var circleMarker = L.circleMarker([markerJson.latitude, markerJson.longitude], 
+            {stroke: true, fillOpacity: 0.75, radius: 16})
+            .addTo(map);
         circleMarker.bindPopup("<div class=\"text-center\"><p class=\"lead\">" + 
             markerJson.message + 
             "</p><p> Memo Date: " +
             markerJson.creation_date +
             "</p></div>");
     }
+    placedMarkers[markerJson.marker_id] = circleMarker;
 }
 
-var marker; // Marker representing user location
-// Places a marker at the user"s geospatial position on the map
+// Adds a link to a memo to the "nearby" dropdown menu
+function addDropdownItem(markerJson) {
+    shortenedText = shortenText(markerJson.message, 20); // Truncate message to 20 characters
+    if (markerJson.author_hash) { // Current user is author of marker: add to TOP of list
+        $("#nearbyMenu").prepend(
+            $("<a/>")
+            .attr("href", "#")
+            .addClass("dropdown-item font-weight-bold")
+            .append($("<strong/>").text(shortenedText))
+            .click(function(){
+                placedMarkers[markerJson.marker_id].fire("click");
+                map.setView([markerJson.latitude, markerJson.longitude], 19);
+            })
+        );
+    } else { // Current user is not author of marker
+        $("#nearbyMenu").append(
+            $("<a/>")
+            .attr("href", "#")
+            .addClass("dropdown-item font-weight-bold")
+            .text(shortenedText)
+            .click(function(){
+                placedMarkers[markerJson.marker_id].fire("click");
+                map.setView([markerJson.latitude, markerJson.longitude], 19);
+            })
+        );
+    }
+}
+
+// If text is longer than length, truncate to (length - 3) and add ...
+// Requires that length is longer than 3
+function shortenText(longText, length) {
+    if (longText.length > length) {
+        return longText.substring(0, length - 3) + "...";
+    } else {
+        return longText;
+    }
+}
+
+var userLocationMarker; // Marker representing user location
+
+// Places a marker at the user's geospatial position on the map
 function updateUserLocation(locationEvent) {
     lastKnownLocation = locationEvent; // Update last known location
-    if (!marker) {
-        marker = L.marker([locationEvent.latitude, locationEvent.longitude], {interactive: false, bubblingMouseEvents: true});
-        map.addLayer(marker);
-    } else {
-        map.removeLayer(marker); // Remove marker so we can redraw it
-        marker = L.marker([locationEvent.latitude, locationEvent.longitude], {interactive: false, bubblingMouseEvents: true});
-        map.addLayer(marker);
+    if (userLocationMarker) {
+        map.removeLayer(userLocationMarker); // Remove marker so we can redraw it
     }
+    userLocationMarker = L.marker([locationEvent.latitude, locationEvent.longitude], {interactive: false, bubblingMouseEvents: true});
+    map.addLayer(userLocationMarker);
 }
 
 // Recenter the map
